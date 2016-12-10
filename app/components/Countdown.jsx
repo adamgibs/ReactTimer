2bf5cc0d3b8c846045073e7864771bd6d6b03905
Adam Gibson <adamgibs@outlook.com>
2016-12-10 15:25:17 -0600

    Add Timer and Countdown components

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Countdown = () => {
+  return(
+    <p>This is the countdown.</p>
+  )
+}
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Timer = () => {
+  return(
+    <p>This is the timer.</p>
+  )
+}
+
+module.exports = Timer;
